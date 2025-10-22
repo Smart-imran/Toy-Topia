a825a9a642ccb351d44fb60f385a88bff940ee64
Smart-imran <rst.imran4@gmail.com>
2025-10-23 00:03:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-5xl bg-green-400 text-center text-white'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
