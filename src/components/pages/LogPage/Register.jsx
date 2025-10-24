a22324da537f48abf2761b3cb56355b80a863de8
Smart-imran <rst.imran4@gmail.com>
2025-10-25 02:07:16 +0600

    some change

--- src/components/pages/LogPage/Register.jsx
+++ src/components/pages/LogPage/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import Swal from "sweetalert2";
 import { AuthContext } from "../../../provider/AuthProvider";
 
